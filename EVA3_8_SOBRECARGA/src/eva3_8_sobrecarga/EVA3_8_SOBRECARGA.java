44cbf0e66f9701714b2c2b5d0eec77ee22aba699
diego-rogelio <diegorogelioph07@gmail.com>
2025-11-14 16:16:45 -0600

    Add files via upload

---
+++ EVA3_8_SOBRECARGA/src/eva3_8_sobrecarga/EVA3_8_SOBRECARGA.java
@@ -0,0 +1,18 @@
+
+package eva3_8_sobrecarga;
+
+import java.awt.BorderLayout;
+
+public class EVA3_8_SOBRECARGA {
+
+    
+    public static void main(String[] args) {
+        System.out.println("hola");
+        System.out.println(100);
+        System.out.println(1.01);
+        System.out.println(true);
+        System.out.println('c');
+        System.out.println(args);
+    }
+    
+}
